8ab701bb8eb6327d9da16b8c828674c19355d427
larsmostue <mostuelars@gmail.com>
2024-11-25 11:18:37 +0100

    Add project files.

---
+++ 2-2_Random-Hobby_Oppgave-1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _2_2_Random_Hobby_Oppgave_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random arbitrary = new Random();
+            while (true)
+            {
+                var RandomChoice = arbitrary.Next(0, 5);
+
+                Console.WriteLine("What's your name?");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"Excellent {name}! Your new hobby is now: ");
+
+                if (RandomChoice == 0)
+                    Console.WriteLine("Fishing");
+                else if (RandomChoice == 1)
+                    Console.WriteLine("Influencer");
+                else if (RandomChoice == 2)
+                    Console.WriteLine("Basejumping");
+                else if (RandomChoice == 3)
+                    Console.WriteLine("Formula 1 Driver");
+                else if (RandomChoice == 4)
+                    Console.WriteLine("Beer Brewing");
+            }
+        }
+    }
+}
